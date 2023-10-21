27b4e6229b68cd9f24b54104437a7453020c1630
lukas <lkab05@hotmail.com>
2023-10-21 14:19:43 -0300

    initial commit

---
+++ src/test/java/br/com/lucasalves/picpaydesafio/PicpayDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasalves.picpaydesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
