2e5c0864a6e2da2f7db6c4f6a1377e017b85e940
lukas <lkab05@hotmail.com>
2023-10-21 15:49:04 -0300

    add service layer

---
+++ src/main/java/br/com/lucasalves/picpaydesafio/service/TransactionService.java
@@ -0,0 +1,77 @@
+package br.com.lucasalves.picpaydesafio.service;
+
+
+import br.com.lucasalves.picpaydesafio.domain.transaction.Transaction;
+import br.com.lucasalves.picpaydesafio.domain.transaction.TransactionDTO;
+import br.com.lucasalves.picpaydesafio.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class TransactionService {
+
+    private TransactionRepository repository;
+
+    private UserService userService;
+
+    private RestTemplate restTemplate;
+
+    private NotificationService notificationService;
+
+    public TransactionService() {
+    }
+    @Autowired
+    public TransactionService(TransactionRepository repository, UserService userService, RestTemplate restTemplate, NotificationService notificationService) {
+        this.repository = repository;
+        this.userService = userService;
+        this.restTemplate = restTemplate;
+        this.notificationService = notificationService;
+    }
+
+
+    public Transaction createTransaction(TransactionDTO dto) throws Exception {
+
+        var payer = this.userService.findUserById(dto.payerId());
+        var payee = this.userService.findUserById(dto.payeeId());
+
+        userService.validateUser(payer, dto.amount());
+
+        boolean isAuthorize = this.authorizeTransaction();
+
+        if (!isAuthorize) {
+            throw new RuntimeException("Transaction not authorized");
+        }
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setAmount(dto.amount());
+        newTransaction.setPayer(payer);
+        newTransaction.setPayee(payee);
+        newTransaction.setTransactionTime(LocalDateTime.now());
+
+        payer.setBalance(payer.getBalance().subtract(dto.amount()));
+        payee.setBalance(payee.getBalance().add(dto.amount()));
+
+        this.repository.save(newTransaction);
+        this.userService.saveUser(payer);
+        this.userService.saveUser(payee);
+
+        notificationService.sendNotification(payer, "Transaction made successfully");
+        notificationService.sendNotification(payee, "You received a transaction");
+
+        return newTransaction;
+    }
+
+    private boolean authorizeTransaction() {
+        var response = restTemplate.getForEntity("https://run.mocky.io/v3/9b89b419-a2f7-4885-aa86-5ddcea24d520", Map.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            String message = response.getBody().get("message").toString();
+            return message.equalsIgnoreCase("Autorizado");
+        } else return false;
+    }
+}
---
+++ src/main/java/br/com/lucasalves/picpaydesafio/service/UserService.java
@@ -0,0 +1,55 @@
+package br.com.lucasalves.picpaydesafio.service;
+
+import br.com.lucasalves.picpaydesafio.domain.user.User;
+import br.com.lucasalves.picpaydesafio.domain.user.UserType;
+import br.com.lucasalves.picpaydesafio.domain.user.UserDTO;
+import br.com.lucasalves.picpaydesafio.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private UserRepository repository;
+
+    public UserService() {
+    }
+
+    @Autowired
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+
+    public User createUser(UserDTO user) {
+        User newUser = new User(user);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
+    }
+
+    public User findUserById(Long id) throws Exception {
+       return this.repository.findById(id).orElseThrow(() -> new Exception("User not found"));
+    }
+
+    public boolean validateUser(User payer, BigDecimal amount) {
+        if (payer.getUserType() == UserType.MERCHANT) {
+            throw new RuntimeException("Merchant can't make transactions");
+        }
+
+        if (payer.getBalance().compareTo(amount) < 0) {
+            throw new RuntimeException("Insufficient funds");
+        }
+
+        return true;
+    }
+}
